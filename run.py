527dd2bd593a47bee87b1b319e69c5199872a28b
nfepb <89026917+nfepb@users.noreply.github.com>
2022-07-06 20:42:28 +0100

    Add function structure to py

--- run.py
+++ run.py
@@ -5,6 +5,20 @@ PLAYER_BOARD = [[' '] * 9 for x in range(9)]
 
 convert_letters_to_numbers_for_coordinates = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E':4, 'F': 5, 'G': 6, 'H': 7, 'I': 8}
 
+# Definition of the functions to launch the games
+# Represent the boardgame for the player and computer
+# Create and add the ships on both boards
+# Allow player input to select where to place a hit to find computer ship
+# Keep track of the number of ship hit
+
 def board_representation():
     pass
 
+def populate_ships_on_board():
+    pass
+
+def player_select_coordinates():
+    pass
+
+def track_number_ship_hit():
+    pass
