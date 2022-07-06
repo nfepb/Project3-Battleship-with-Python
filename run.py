e42c72896c01cb59fc388d98c453e5ec01c75449
nfepb <89026917+nfepb@users.noreply.github.com>
2022-07-06 20:33:24 +0100

    Add variable for converting letters to numbers for coordinates

--- run.py
+++ run.py
@@ -2,3 +2,9 @@
 
 HIDDEN_BOARD = [[' '] * 9 for x in range(9)]
 PLAYER_BOARD = [[' '] * 9 for x in range(9)]
+
+convert_letters_to_numbers_for_coordinates = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E':4, 'F': 5, 'G': 6, 'H': 7, 'I': 8}
+
+def board_representation():
+    pass
+
